3bfd739e838546148ceb77452a7035c2bb1cf5c8
chidanand582001 <chidanand582001@gmail.com>
2021-05-19 22:42:26 +0530

    First App by Using React...

---
+++ src/MyComponents/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+        <i>
+        <p>  <h4>Make it fast to add and organize tasks.</h4> Ideally, a task is added and categorized in a couple taps or keystrokes.
+
+<h4>Offer multiple ways to organize your tasks.</h4> Tags, lists, projects, and due dates are all helpful, and the best apps offer at least a few categories like this.
+
+<h4>Remind you about self-imposed deadlines.</h4> Notifications, widgets, emails—the best applications make it obvious when something needs to be completed.
+
+<h4>Offer clean user interfaces.</h4> Well-designed to-do apps fit into your workflow so you can get back to what you're supposed to be doing.
+
+<h4>Sync between every platform you use.</h4> Which platforms will depend on what you personally use, but we didn't consider anything that doesn't sync between desktop and mobile.</p> 
+</i> </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; chidanand582001@gmail.com
+            </p>
+        </footer>
+    )
+}
